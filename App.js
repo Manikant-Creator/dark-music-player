f29efd91f5c45009e43775d78f7c8fdb1a6432e8
Manikant-Creator <manikantrajsarve2020@gmail.com>
2025-06-29 00:00:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>🎧 Dark Music Player 🎵</Text>
+      <Text style={styles.subtitle}>Listen in Dark</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    color: '#aaa',
+    fontSize: 16,
+  },
+});
